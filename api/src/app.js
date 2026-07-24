605d9d80ae85a0885515db78da8665f63a836893
J-ley644 <javansbradley@gmail.com>
2026-07-24 16:13:36 +0300

    Add API home route

--- api/src/app.js
+++ api/src/app.js
@@ -5,33 +5,55 @@ import authRoutes from "./routes/authRoutes.js";
 import deploymentRoutes from "./routes/deploymentRoutes.js";
 import clientBotRoutes from "./routes/clientBotRoutes.js";
 
-
 const app = express();
-
 
 app.use(cors());
 
 app.use(express.json());
 
+/*
+|--------------------------------------------------------------------------
+| Root Route
+|--------------------------------------------------------------------------
+*/
 
+app.get("/", (req, res) => {
+
+    res.json({
+
+        success: true,
+
+        application: "JLEY-XMD",
+
+        status: "ONLINE",
+
+        version: "1.0.0",
+
+        message: "Welcome to the JLEY-XMD API"
+
+    });
+
+});
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
 
 app.use(
     "/api/auth",
     authRoutes
 );
 
-
 app.use(
     "/api/deployments",
     deploymentRoutes
 );
 
-
 app.use(
     "/api/client",
     clientBotRoutes
 );
-
-
 
 export default app;
